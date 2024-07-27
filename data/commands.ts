eae5a9311dcc65ededb4aac2bcb524cc99bf8ee6
yhoungdev <yhoungdev@gmail.com>
2024-07-27 05:19:00 +0100

    chore: updated commands descriptions 🖊️🖊️🖊️

--- data/commands.ts
+++ data/commands.ts
@@ -2,6 +2,7 @@ import { program } from "commander";
 import { generateKeypairs } from "../utils/generateWallet";
 import { clusterApiUrl, Connection } from "@solana/web3.js";
 import { sendToken } from "../utils/sendSolana";
+import { displayAddress } from "../utils/showAddress";
 
 export const COMMANDS = [
   {
@@ -11,9 +12,21 @@ export const COMMANDS = [
     action: generateKeypairs,
   },
   {
+    name: "signin",
+    command: "signin",
+    description: "Sign in a new Solana wallet",
+    action: generateKeypairs,
+  },
+  {
     name: "send",
     command: "send <address> <amount>",
     description: "Send a token to another wallet",
-    action: generateKeypairs,
+    action: sendToken,
+  },
+  {
+    name: "display-address",
+    command: "display",
+    description: "Display my solana address",
+    action: displayAddress,
   },
 ];
